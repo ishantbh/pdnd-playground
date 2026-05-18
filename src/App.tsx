671df6f736c109dafbf3ca346222876e355ee12d
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-05-18 13:43:11 +0530

    setup theme toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { ThemeToggle } from '@/components/theme/theme-toggle'
+
 export default function App() {
   return (
     <div>
       <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+      <ThemeToggle />
     </div>
   )
 }
